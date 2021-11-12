b7d637d12c3d49abfe001ad4e27184558e044174
ingandres00 <juliochaparro8706@gmail.com>
2021-11-12 10:34:43 -0500

    Add files via upload

---
+++ entregableML.py
@@ -0,0 +1,11 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+%matplotlib inline
+
+!wget -O loan_train.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/FinalModule_Coursera/data/loan_train.csv
